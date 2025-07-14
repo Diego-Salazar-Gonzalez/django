9923fd8828c49ca845c66310571b97e583716e40
Diego Salazar <diego.salazar@gmail.com>
2025-07-14 06:09:55 -0500

    final creo

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
-
+from django.contrib.messages import constants as messages
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -68,9 +68,17 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
 ]
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+MESSAGE_TAGS = {
+    messages.DEBUG: 'bg-gray-100 text-gray-800',
+    messages.INFO: 'bg-blue-100 text-blue-800',
+    messages.SUCCESS: 'bg-green-100 text-green-800',
+    messages.WARNING: 'bg-yellow-100 text-yellow-800',
+    messages.ERROR: 'bg-red-100 text-red-800',
+}
 if DEBUG:
     # Add django_browser_reload middleware only in DEBUG mode
     MIDDLEWARE += [
